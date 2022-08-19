6d80f1a0119d4e32f0fba9e78ff44aa71d51df22
ttershak <ttershak@gmail.com>
2022-08-19 14:48:58 +0300

    adding information about us

---
+++ app/controllers/about_us_controller.rb
@@ -0,0 +1,9 @@
+class AboutUsController < ApplicationController
+  def about
+
+  end
+
+  def find_us
+
+  end  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root "kosiv#startpage"
+
+  get "abaut" => "about_us#about"
+  get "contacts" => "about_us#find_us"
 end

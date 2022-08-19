1a648564d64880d7cf6a18ac3ad3e3ce35bd78a8
ttershak <ttershak@gmail.com>
2022-08-19 13:29:38 +0300

    change startpage

---
+++ app/controllers/kosiv_controller.rb
@@ -0,0 +1,5 @@
+class KosivController < ApplicationController
+  def startpage
+    
+  end
+end
---
+++ test/controllers/kosiv_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class KosivControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
